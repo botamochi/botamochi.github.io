41ee5ca60d8c4a00528067f7a948705f637d4102
botamochi <botamochi0914@yahoo.co.jp>
2016-11-25 08:05:17 +0900

    メニューの「一覧」を「リスト」に変更

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,6 @@
-// メニューを自動で追加
 window.onload = function()
 {
+    // 横のメニューを追加
     var div = document.getElementsByClassName("wrapper")[0];
     var menu = document.createElement("div");
     menu.classList.add("side-menu");
@@ -15,13 +15,13 @@ window.onload = function()
     // 記事のリスト
     var ul2 = document.createElement("ul");
     if (pagetype == "creation") {
-	ul2.innerHTML = '<li><b>&lt;&lt; 制作物一覧 &gt;&gt;</b>';
+	ul2.innerHTML = '<li><b>&lt;&lt; 制作物リスト &gt;&gt;</b>';
 	ul2.innerHTML += '<hr style="margin: 0px 10px 20px 10px;">';
 	ul2.innerHTML += '<li><a href="http://blogs.yahoo.co.jp/nazonootokoj/19982699.html">初代ギア用サンドバッグ</a></li>';
 	ul2.innerHTML += '<li><a href="' + path + 'creation/irdigimon.html">初代ギア無線化回路</a></li>';
 	ul2.innerHTML += '<li><a href="' + path + 'creation/taberumon.html">デスクトップアクセサリ</a></li>';
     } else if (pagetype == "analysis") {
-	ul2.innerHTML = '<li><b>&lt;&lt; 解析データ一覧 &gt;&gt;</b>';
+	ul2.innerHTML = '<li><b>&lt;&lt; 解析データリスト &gt;&gt;</b>';
 	ul2.innerHTML += '<hr style="margin: 0px 10px 20px 10px;">';
 	ul2.innerHTML += '<li><a href="' + path + 'analysis/origin.html">初代デジタルモンスター</a></li>';
 	ul2.innerHTML += '<li><a href="' + path + 'analysis/dlink.html">D-Link</a></li>';
